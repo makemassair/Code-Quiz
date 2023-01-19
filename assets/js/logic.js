13a948ded1117cbd30a2e8e5a59cf32799e880f0
Michael Samaras <mike@makemassair.co.uk>
2023-01-19 08:53:26 +0000

    Qs working. Score working. Endscreen working.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,7 +6,7 @@ const questionChoices = document.querySelector("#choices");
 const timer = document.querySelector(".timer");
 const timerCountdown = document.querySelector("#time");
 const endQuiz = document.querySelector("#end-screen");
-const score = document.querySelector("#final-score");
+const finalScore = document.querySelector("#final-score");
 const playerName = document.querySelector("#initials");
 const confirmName = document.querySelector("#submit");
 const quizFeedback = document.querySelector("#feedback");
@@ -14,69 +14,87 @@ const quizFeedback = document.querySelector("#feedback");
 // Declared Variables
 var activeQuestion;
 var answerButton;
+var timerInterval;
 var questionIndex = 0;
+var currentScore = 0;
 totalPlayTime = questionsToAsk.length * 10 + 1;
 
 // Time Countdown
 function timerCount() {
     // Sets interval in variable
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         totalPlayTime--;
         timerCountdown.textContent = totalPlayTime;
   
-        if(totalPlayTime === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
+        if(totalPlayTime <= 0) {
         // Calls function to create and append image
         endGame();
       }
-  
     }, 1000);
   };
 
 function endGame() {
+    clearInterval(timerInterval);
     question.setAttribute("class", "hide");
     endQuiz.removeAttribute("class", "hide");
-}
+    finalScore.innerText = currentScore;}
 
 // Declare a function to start the Quiz
 beginQuiz.addEventListener("click", function() {
     console.log(`The "start button" has been clicked`);
+    currentScore = 0;
+    questionIndex = 0;
     quizIntro.setAttribute("class", "hide");
     question.removeAttribute("class", "hide");
     timerCount();
     printQ();
-    scoring();
 });
 
 questionChoices.addEventListener("click", function(event) {
     var button = event.target;
-    console.log(button);
+    scoring(button);
 });
 
 //  Declare a function to render questions.
 function printQ() {
     // Clear choices from any previous question
+    if (questionIndex === questionsToAsk.length) {
+        endGame();
+        return;
+    }
     questionChoices.textContent = "";
     activeQuestion = questionsToAsk[questionIndex];
     questionTitle.textContent = activeQuestion.question;
     for (let i = 0; i < activeQuestion.choices.length; i++) {
         var buttonChoice = document.createElement("button");
         buttonChoice.setAttribute("class", "answerButton");
-        buttonChoice.textContent = i + ". " + activeQuestion.choices[i];
+        buttonChoice.textContent = (i+1) + ". " + activeQuestion.choices[i];
         buttonChoice.dataset.isCorrect = i === activeQuestion.answer;
         questionChoices.appendChild(buttonChoice);
     }
+    questionIndex++;
 };
 
-
-function scoring() {
-    if (document.querySelectorAll("[data-is-correct='false'")) {
-        totalPlayTime-- *5;
+function scoring(button) {
+    var score = button.dataset.isCorrect;
+    if (score == "false") {
+        totalPlayTime -= 5;
+        console.log(totalPlayTime);
     }
     else {
-        printQ();
-    }
-    console.log(totalPlayTime);
-    
+        currentScore++;
+        }
+    printQ();
 };
+
+confirmName.addEventListener("click", function() {
+    var initials = playerName.textContent;
+    var highScores = JSON.parse(localStorage.getItem('playerScore'));
+    if (highScores === null) {
+        highScores = [];
+    }
+    highScores.push({initials:initials, finalScore:currentScore});
+    localStorage.setItem('playerScore', JSON.stringify(highScores));
+    quizIntro.removeAttribute("class", "hide");
+    endQuiz.setAttribute("class", "hide");
+});
---
+++ assets/js/scores.js
@@ -0,0 +1,6 @@
+var scoringSystem = [
+    {
+        initials:
+        finalScore:
+    }
+]
